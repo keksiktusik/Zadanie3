b1c7b813bc1803a8fc83a25640d5fa25b93e59e9
Marta <martaprzyszowa@gmail.com>
2024-11-12 10:33:51 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 
 int main() {
 
+std::vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
+    MergeSort::sort(arr);
+
+    std::cout << "Sorted array: ";
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+    
     return 0;
 
 }
